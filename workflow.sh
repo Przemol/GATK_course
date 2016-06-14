bccb846b5f4edfa900c1c2ee33fe82b3b22b45ab
Przemol <Przemol@users.noreply.github.com>
2016-06-14 11:14:27 +0100

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -13,7 +13,7 @@ java -jar $GATK -T HaplotypeCaller \
   -o sandbox/NA12878_wgs_20_HC_calls.vcf \
   -L 20:10,000,000-10,200,000
 
-#View realigned reads and assembled haplotypes 
+# View realigned reads and assembled haplotypes 
 java -jar $GATK -T HaplotypeCaller \
   -R ref/human_g1k_b37_20.fasta \
   -I bams/exp_design/NA12878_wgs_20.bam \
@@ -23,3 +23,12 @@ java -jar $GATK -T HaplotypeCaller \
   -disableOptimizations \
   -L 20:10,002,371-10,002,546 \
   -ip 100
+  
+# Run more samples 
+java -jar $GATK -T HaplotypeCaller \
+  -R ref/human_g1k_b37_20.fasta \
+  -I bams/exp_design/NA12878_wgs_20.bam \
+  -I bams/trio-calling/NA12877_wgs_20.bam \
+  -I bams/trio-calling/NA12882_wgs_20.bam \
+  -o sandbox/NA12878_wgs_20_HC_jointcalls.vcf \
+  -L 20:10,000,000-10,200,000

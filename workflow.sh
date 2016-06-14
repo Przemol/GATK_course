a281531bcf531633daf40b20e6e70494ab12bd5f
Przemol <Przemol@users.noreply.github.com>
2016-06-14 11:04:16 +0100

    Create workflow.sh

---
+++ workflow.sh
@@ -0,0 +1,25 @@
+# Call variants with a position­based caller: UnifiedGenotyper 
+java -jar $GATK -T UnifiedGenotyper \
+  -R ref/human_g1k_b37_20.fasta \
+  -I bams/exp_design/NA12878_wgs_20.bam \
+  -o sandbox/NA12878_wgs_20_UG_calls.vcf \
+  -glm BOTH \
+  -L 20:10,000,000-10,200,000 
+
+# Call variants with HaplotypeCaller 
+java -jar $GATK -T HaplotypeCaller \
+  -R ref/human_g1k_b37_20.fasta \
+  -I bams/exp_design/NA12878_wgs_20.bam \
+  -o sandbox/NA12878_wgs_20_HC_calls.vcf \
+  -L 20:10,000,000-10,200,000
+
+#View realigned reads and assembled haplotypes 
+java -jar $GATK -T HaplotypeCaller \
+  -R ref/human_g1k_b37_20.fasta \
+  -I bams/exp_design/NA12878_wgs_20.bam \
+  -o sandbox/NA12878_wgs_20_HC_calls_debug.vcf \
+  -bamout sandbox/NA12878_wgs_20.HC_out.bam \
+  -forceActive \
+  -disableOptimizations \
+  -L 20:10,002,371-10,002,546 \
+  -ip 100
